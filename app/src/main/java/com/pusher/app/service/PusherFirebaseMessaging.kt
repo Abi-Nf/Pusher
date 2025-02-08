691f91d9c9abb4547650bd595774e55b2eca6e35
kirisaki-vk <kirisaki.vk@gmail.com>
2025-02-08 19:09:34 +0300

    feat: implemented fcm messages handling

--- app/src/main/java/com/pusher/app/service/PusherFirebaseMessaging.kt
+++ app/src/main/java/com/pusher/app/service/PusherFirebaseMessaging.kt
@@ -1,9 +1,96 @@
 package com.pusher.app.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.ui.res.painterResource
+import androidx.core.app.NotificationCompat
+import androidx.core.graphics.drawable.IconCompat
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.pusher.app.MainActivity
+import com.pusher.app.NOTIFICATION_CHANNEL_ID
+import com.pusher.app.NOTIFICATION_CHANNEL_NAME
+import com.pusher.app.R
+import com.pusher.app.dataStore
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
+private const val TAG = "MyFirebaseMsgService"
 class PusherFirebaseMessaging : FirebaseMessagingService() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        GlobalScope.launch {
+            saveFCMToken(token)
+        }
+    }
+
+    private suspend fun saveFCMToken(token: String) {
+        val fcmTokenKey = stringPreferencesKey("fcm_token")
+        baseContext.dataStore.edit {
+            it[fcmTokenKey] = token
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d(TAG, "From: ${message.from}")
+
+        // Check if message contains a data payload.
+        if (message.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${message.data}")
+
+        }
+
+        // Check if message contains a notification payload.
+        message.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            it.body?.let { body -> sendNotification(body) }
+        }
+    }
+
+    private fun sendNotification(messageBody: String) {
+        val requestCode = 0
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE,
+        )
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Pusher notification service")
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT,
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
